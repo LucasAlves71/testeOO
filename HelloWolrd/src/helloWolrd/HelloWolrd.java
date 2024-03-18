7854cf5cfb037f05162ab9e135c520a375159da6
lucas <lucas@DESKTOP-DM4MB1T>
2024-03-18 17:33:36 -0300

    Primeiro Commit

---
+++ HelloWolrd/src/helloWolrd/HelloWolrd.java
@@ -0,0 +1,11 @@
+package helloWolrd;
+
+public class HelloWolrd {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Wolrd!!");
+		System.out.println("Esse é o meu primeiro programa em Java!!");
+
+	}
+
+}
